2b5ce38f5e71513862be3b8f2b59f416bbe96a6d
Billy Alvear <alvearbilly@gmail.com>
2026-03-31 22:28:30 -0500

    add envs and config envs

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
@@ -22,14 +22,14 @@ export class CheckService implements CheckServiceUseCase {
 
       const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
       this.logRepository.saveLog(log);
-      this.successCallback();
+      this.successCallback && this.successCallback();
 
       return true;
     } catch (error) {
       const errorMessage = `${url} its not ok. ${error}`;
       const log = new LogEntity(errorMessage, LogSeverityLevel.high);
       this.logRepository.saveLog(log);
-      this.errorCallback(errorMessage);
+      this.errorCallback && this.errorCallback(errorMessage);
 
       return false;
     }
