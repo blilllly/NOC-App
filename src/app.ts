64efb9183f6b5803f342ecf3de4f5c14a1ddccd3
Billy Alvear <alvearbilly@gmail.com>
2026-04-10 06:59:49 -0500

    mongo connected

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import { envs } from './config/plugins/envs.plugins';
+import { MongoDatabase } from './data/mongodb';
 import { Server } from './presentation/server';
 
 (async () => {
   await main();
 })();
 
-function main() {
-  Server.start();
+async function main() {
+  await MongoDatabase.connet({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+  // Server.start();
   // console.log('server started..');
 }
