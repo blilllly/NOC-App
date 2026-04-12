10c16dd7f7b65d4c88014eaa641b489433739121
Billy Alvear <alvearbilly@gmail.com>
2026-04-11 19:11:19 -0500

    fsDatasource when content is empty

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,3 +1,4 @@
+import { LogSeverityLevel } from '../domain/entities/log.entity';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
@@ -15,7 +16,7 @@ const logRepository = new LogRepositoryImpl(
 const emailService = new EmailService();
 
 export class Server {
-  public static start() {
+  public static async start() {
     console.log('Server started...');
 
     // Mandar email
@@ -35,14 +36,17 @@ export class Server {
     //   `,
     // });
 
-    CronService.createJob('*/5 * * * * *', () => {
-      const url = 'https://google.com';
+    const logs = await logRepository.getLogs(LogSeverityLevel.low);
+    console.log(logs);
 
-      new CheckService(
-        logRepository,
-        () => console.log(`${url} is ok`),
-        (error) => console.log(error),
-      ).execute(url);
-    });
+    // CronService.createJob('*/5 * * * * *', () => {
+    //   const url = 'https://google.com';
+
+    //   new CheckService(
+    //     logRepository,
+    //     () => console.log(`${url} is ok`),
+    //     (error) => console.log(error),
+    //   ).execute(url);
+    // });
   }
 }
