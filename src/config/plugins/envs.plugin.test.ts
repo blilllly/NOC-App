18cd14d7e17be6b3e84793340c62acf6c8215a5f
Billy Alvear <alvearbilly@gmail.com>
2026-04-15 06:41:37 -0500

    fix the containers error: docker-compose.test only replace, now create a new container for test

--- src/config/plugins/envs.plugin.test.ts
+++ src/config/plugins/envs.plugin.test.ts
@@ -2,6 +2,7 @@ import { envs } from './envs.plugins';
 
 describe('envs.plugin.ts', () => {
   it('should return env options', () => {
+    console.log(envs);
     expect(true).toBeTruthy();
   });
 });
